efd9d59d46e034efd32a413d3dd2f2f2bc357126
helloquanghuy <helloqhuy@gmail.com>
2021-06-11 13:18:18 +0700

    create table khachhang and catch event when login add to webservice

--- app/src/main/java/com/example/project_thuc_tap/activity/LoginScreen.java
+++ app/src/main/java/com/example/project_thuc_tap/activity/LoginScreen.java
@@ -1,6 +1,7 @@
 package com.example.project_thuc_tap.activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,7 +10,15 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.project_thuc_tap.R;
+import com.example.project_thuc_tap.ulities.Server;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -23,6 +32,9 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -64,6 +76,7 @@ public class LoginScreen extends AppCompatActivity {
             String email = edtEmailPhone.getText().toString().trim();
             String pass = edtPassWord.getText().toString().trim();
             SignInNormal(email,pass);
+
         });
 
 
@@ -78,6 +91,29 @@ public class LoginScreen extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Toast.makeText(LoginScreen.this, "đăng nhập thành công !", Toast.LENGTH_LONG).show();
                             startActivity(new Intent(LoginScreen.this, MainActivity.class));
+                            //khi đăng nhập
+                            RequestQueue  requestQueue = Volley.newRequestQueue(getApplicationContext());
+                            StringRequest stringRequest = new StringRequest(Request.Method.POST, Server.urlGuest, new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+
+                                }
+                            }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+
+                                }
+                            }){
+                                @Nullable
+                                @Override
+                                protected Map<String, String> getParams() throws AuthFailureError {
+                                    HashMap<String, String> hashMap = new HashMap<>();
+                                    hashMap.put("email", email);
+                                    return hashMap;
+                                }
+                            };
+
+                            requestQueue.add(stringRequest);
 
                         } else {
                             // If sign in fails, display a message to the user.
--- app/src/main/java/com/example/project_thuc_tap/ulities/Server.java
+++ app/src/main/java/com/example/project_thuc_tap/ulities/Server.java
@@ -3,4 +3,5 @@ package com.example.project_thuc_tap.ulities;
 public class Server {
     public static String localHost = "192.168.54.106";
     public static String urlProductNew = "http://" + localHost + "/androidwebservice/getloaiSPmoinhat.php";
+    public static String urlGuest = "http://" + localHost + "/androidwebservice/luuthongtinkhachhang.php";
 }
